b57cc98a965ff4b85e51a2c0c0c6ddf95eb77e0a
Anne JOUVE <annejouve@iMac-de-Anne.local>
2021-07-27 08:44:49 +0200

    Création de la boucle pour le choix des vernis

--- produit.js
+++ produit.js
@@ -5,51 +5,11 @@ const queryString_url_id = window.location.search;
 const urlSearchParams = new URLSearchParams(queryString_url_id);
 const selectedId = urlSearchParams.get("id");
 
-/* Définition d'un modèle pour le meuble sélectionné */
-/*class SelectedFurniture {
-    constructor(jsonSelectedFurniture){
-        jsonSelectedFurniture && Object.assign(this, jsonSelectedFurniture);
-    }
-    getFormatedSelectedPrice(selectedFurniture){
-        return this.price / 100;
-    }
-}*/
-
-/* Affichage du meuble sélectionné */
-/*fetch(`http://localhost:3000/api/furniture/${selectedId}`)
-    .then( response => response.json())
-    .then( selectedFurniture => {
-        new SelectedFurniture(jsonSelectedFurniture);
-        document.querySelector("#selected-furniture").innerHTML = `<div class="col-xxl-10 m-xxl-auto">
-                                                                        <div class="card bg-light text-center mb-4">
-                                                                        <img src="${selectedFurniture.imageUrl}" class="card-img-top" alt="Photo du meuble en chêne Cross Table">
-                                                                            <div class="card-body">
-                                                                            <h5 class="card-title">${selectedFurniture.name}</h5>
-                                                                            <p class="card-text">${selectedFurniture.description}</p>
-                                                                            <p class="card-text fw-bold">${furniture.getFormatedSelectedPrice()} €</p>
-                                                                                <div class="dropdown mb-3 col-2">
-                                                                                    <button class="btn btn-outline-primary dropdown-toggle text-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                                                                    Vernis
-                                                                                    </button>
-                                                                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                                                                        <li><a class="dropdown-item" href="#">Tan</a></li>
-                                                                                        <li><a class="dropdown-item" href="#">Chocolate</a></li>
-                                                                                        <li><a class="dropdown-item" href="#">Black</a></li>
-                                                                                        <li><a class="dropdown-item" href="#">White</a></li>
-                                                                                    </ul>
-                                                                                </div>
-                                                                                    <a href="panier.html" class="btn btn-primary"><i class="fas fa-shopping-basket"></i>&nbsp;&nbsp;Ajouter au panier</a>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>`;
-    });*/
-
+/* Affichage du meuble */
     const promesse = fetch(`http://localhost:3000/api/furniture/${selectedId}`);
     promesse
         .then((response) => {
-            console.log(response);
             const furnitureData = response.json(); /*j'enregistre dans une variable les données de la réponse de la promesse transformées en json. Renvoie une nouvelle promesse*/
-            console.log(furnitureData);
             furnitureData.then((meuble) => { /* je récupère le tableau correspondant à mon meuble */
                 const id = meuble._id;
                 const description = meuble.description;
@@ -57,6 +17,10 @@ const selectedId = urlSearchParams.get("id");
                 const name = meuble.name;
                 const price = meuble.price / 100 + " €";
                 const varnish = meuble.varnish;
+                let structureVarnish = [];
+                for (let i = 0; i < varnish.length; i++) {
+                    structureVarnish += `<li><a class="dropdown-item" href="#">${varnish[i]}</a></li>`;
+                }
                 document.querySelector("#selected-furniture").innerHTML = `<div class="col-xxl-10 m-xxl-auto">
                                                                                 <div class="card bg-light text-center mb-4">
                                                                                 <img src=${imageUrl} class="card-img-top" alt="Photo du meuble en chêne Cross Table">
@@ -68,11 +32,8 @@ const selectedId = urlSearchParams.get("id");
                                                                                             <button class="btn btn-outline-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                                                                             Vernis
                                                                                             </button>
-                                                                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                                                                                <li><a class="dropdown-item" href="#">Tan</a></li>
-                                                                                                <li><a class="dropdown-item" href="#">Chocolate</a></li>
-                                                                                                <li><a class="dropdown-item" href="#">Black</a></li>
-                                                                                                <li><a class="dropdown-item" href="#">White</a></li>
+                                                                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" id="option">
+                                                                                            ${structureVarnish}
                                                                                             </ul>
                                                                                         </div>
                                                                                             <a href="panier.html" class="btn btn-primary"><i class="fas fa-shopping-basket"></i>&nbsp;&nbsp;Ajouter au panier</a>
